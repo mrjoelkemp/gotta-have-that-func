b6e6707d38249b8b175cce6ea3d04430d4ca2b84
Joel Kemp <mrjoelkemp@gmail.com>
2013-10-12 15:54:03 -0400

    A few funcs

---
+++ the-func.js
@@ -0,0 +1,64 @@
+;(function () {
+  'use strict';
+
+  var func = {};
+  window.func = func;
+
+  func.reduce = function (list, iterator, memo) {
+    list.forEach(function (val) {
+      memo = iterator(memo, val);
+    });
+
+    return memo;
+  };
+
+  func.intersection = function () {
+    var firstList = arguments[0],
+        otherLists = [].slice.call(arguments, 1),
+        result = [],
+
+        inOtherLists = function (val, otherLists) {
+          return otherLists.filter(function (list) {
+            return list.indexOf(val) !== -1;
+          }).length === otherLists.length;
+        };
+
+    firstList.forEach(function(val) {
+      if (inOtherLists(val, otherLists) && result.indexOf(val) === -1) {
+        result.push(val);
+      }
+    });
+
+    return result;
+  };
+
+  func.union = function () {
+    var result = [];
+
+    [].forEach.call(arguments, function (sublist) {
+      sublist.forEach(function (val) {
+        if (result.indexOf(val) === -1) result.push(val);
+      });
+    });
+
+    return result;
+  };
+
+  func.zip = function () {
+    var mappings = {};
+
+    [].forEach.call(arguments, function (sublist) {
+      sublist.forEach(function (val, index) {
+        mappings[index] || (mappings[index] = []);
+        mappings[index].push(val);
+      });
+    });
+
+    // Stupid that there's no Object.values
+    return Object.keys(mappings).map(function (key) {
+      return mappings[key];
+    });
+  };
+
+
+})();
